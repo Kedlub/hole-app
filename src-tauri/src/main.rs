83e6ce5b1e95b3d186c47cd1e2fabdb9f0230fbc
Kedlub <jakubkyzr@gmail.com>
2023-12-10 22:22:31 +0100

    Delete unnecessary SVG files and unused code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,12 +5,6 @@ use tauri::Manager;
 use window_shadows::set_shadow;
 #[cfg(any(windows, target_os = "macos"))]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 fn main() {
     tauri::Builder::default().setup(|app| {
         let window = app.get_window("main").expect("Error");
--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Greet from "./lib/Greet.svelte";
 	import Titlebar from "./lib/Titlebar.svelte";
 
 	let showHole = false;
@@ -24,13 +23,3 @@
 		</div>
 	</main>
 </div>
-
-<style>
-	.logo.vite:hover {
-		filter: drop-shadow(0 0 2em #747bff);
-	}
-
-	.logo.svelte:hover {
-		filter: drop-shadow(0 0 2em #ff3e00);
-	}
-</style>
