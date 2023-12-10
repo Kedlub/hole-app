72359c79d24ddd16d3adc04446afedfc3a0f974f
Kedlub <jakubkyzr@gmail.com>
2023-12-10 10:54:18 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import Greet from "./lib/Greet.svelte";
+	import Titlebar from "./lib/Titlebar.svelte";
+
+	let showHole = false;
+</script>
+
+<div class="main-container">
+	<Titlebar />
+	<main class="container" class:hole={showHole}>
+		<div>
+			<h1>Totally normal app</h1>
+			<p>
+				This is a totally normal app. It does nothing special. It's just a
+				regular app.
+			</p>
+		</div>
+		<div class="container-grow">
+			{#if !showHole}
+				<button on:click={() => (showHole = !showHole)}>
+					This is just a regular button
+				</button>
+			{/if}
+		</div>
+	</main>
+</div>
+
+<style>
+	.logo.vite:hover {
+		filter: drop-shadow(0 0 2em #747bff);
+	}
+
+	.logo.svelte:hover {
+		filter: drop-shadow(0 0 2em #ff3e00);
+	}
+</style>
---
+++ src/lib/Titlebar.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { appWindow } from '@tauri-apps/api/window';
+
+    // Close the window when the close button is clicked
+    function handleClose() {
+        appWindow.close();
+    }
+</script>
+
+<style>
+    .titlebar {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        height: 30px;
+        background-color: #333;
+        color: white;
+        padding: 0 10px;
+        width: 100%;
+        user-select: none;
+        box-sizing: border-box;
+    }
+
+    .close-button {
+        cursor: pointer;
+    }
+
+    .close-button:hover {
+        color: #ff3e00;
+    }
+</style>
+
+<div data-tauri-drag-region class="titlebar">
+    <span class="close-button" on:click={handleClose}>X</span>
+</div>
