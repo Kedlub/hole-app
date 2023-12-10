186ac0dfb990e37f9e5916c84b3cd59210ed11a1
Kedlub <jakubkyzr@gmail.com>
2023-12-10 13:04:18 +0100

    Add window shadows

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+use window_shadows::set_shadow;
+#[cfg(any(windows, target_os = "macos"))]
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,8 +12,11 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+    tauri::Builder::default().setup(|app| {
+        let window = app.get_window("main").expect("Error");
+        set_shadow(&window, true).unwrap(); 
+        Ok(())
+    }).invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
